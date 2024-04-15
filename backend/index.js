3d2da61b7480022f5cae6cd9ade44801b75294d7
Marcelo Rigon <marcelo.rigoon@hotmail.com>
2024-04-15 20:29:01 -0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  
+  /* 
+  const { username } = req.body;
+
+  try {
+    const r = await axios.put{
+      "https://api.chatengine.io/users/",
+      {username: username, secret: username, first_name: username},
+      {headers: {"private-key: put the key here"}}
+    };
+
+    return res.status(e.status).json(r.data);
+  
+  } catch (e) {
+    return res.status(e.response.status).json(r.response.data)
+  }
+  return res.json({ username: username, secret: "sha256..." })
+  ;*/
+});
+
+app.listen(3001);
